41ba430d72e7a00b8a003baa1dd806ba965b13c6
lijun <lijun359714823@163.com>
2016-09-07 02:33:30 +0800

    first commit

---
+++ src/main/java/com/ifox/CalculatorServiceApplication.java
@@ -0,0 +1,14 @@
+package com.ifox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class CalculatorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorServiceApplication.class, args);
+	}
+}
